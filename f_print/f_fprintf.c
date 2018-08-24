e6f5a594437fe36d48f1373dfe0d33a3041db8ca
Werner Seegers <wseegers.mauws@gmail.com>
2018-08-24 21:48:51 +0200

    Minor typo

--- f_print/f_fprintf.c
+++ f_print/f_fprintf.c
@@ -1,22 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   f_fprinf.c                                         :+:      :+:    :+:   */
+/*   f_fprintf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 17:11:58 by wseegers          #+#    #+#             */
-/*   Updated: 2018/08/24 21:34:35 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/08/24 21:45:14 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "f_print.h"
-
-int		f_vfprintf(t_file *file, const char *format, va_list ap)
-{
-	return (f_vdprintf(file->fd_out, format, ap));
-}
 
 int		f_fprintf(t_file *file, const char *format, ...)
 {
